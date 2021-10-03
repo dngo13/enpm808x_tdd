8aee72dfa552ca7044173f90ced9b50a3fa6d0bb
ameyakonk <78075049+ameyakonk@users.noreply.github.com>
2021-10-03 18:05:14 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -6,8 +6,8 @@ using std :: cin;
 int main() {
     double pid_var[3] = {0}, sp = 0, cv = 0;                  
     cout << "Enter PID values, setpoint and current velocity";
-    cin >> pid_var[0] >> pid_var[1] >> pid_var[2] >> sp >> cv ;       // Enter kd, ki, kd values
+    cin >> pid_var[0] >> pid_var[1] >> pid_var[2] >> sp >> cv ;         // Enter kd, ki, kd values
     ControllerPID p(pid_var[0] ,pid_var[1], pid_var[2]);                // Create instance for ControllerPID class
-    p.computeVelocity(sp, cv);
+    cout << p.computeVelocity(sp, cv);
     return 0;
 }
