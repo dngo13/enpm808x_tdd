90db49c0ed141b6016762798eff7515e8c1fa36e
ameyakonk <78075049+ameyakonk@users.noreply.github.com>
2021-10-03 17:48:33 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -1,14 +1,11 @@
 // Copyright (C) 2021 by Aditi Ramadwar
 #include <iostream>
 #include <pid_lib.hpp>
-
 using std :: cout;
 using std :: cin;
-using std :: endl;
-
 int main() {
     double pid_var[3] = {0}, sp = 0, cv = 0;                  
-    cout << "Enter PID values, setpoint and current velocity" << endl;
+    cout << "Enter PID values, setpoint and current velocity";
     cin >> pid_var[0] >> pid_var[1] >> pid_var[2] >> sp >> cv ;       // Enter kd, ki, kd values
     ControllerPID p(pid_var[0] ,pid_var[1], pid_var[2]);                // Create instance for ControllerPID class
     p.computeVelocity(sp, cv);
