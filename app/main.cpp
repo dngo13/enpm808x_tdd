6520d47912bd49517e25f319e584db74d0109835
ameyakonk <78075049+ameyakonk@users.noreply.github.com>
2021-10-03 17:03:15 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -6,12 +6,6 @@ using std :: cout;
 using std :: cin;
 
 int main() {
-    /**
-     * To-do in Part 2
-     * Take input for the three gain values, desired velocity and current velocity
-     * 
-     * Call the compute method and print the new velocity value
-     * */
     ControllerPID p(1 ,1, 1);
     double sp = 0, cv = 0;
     cout << "Enter Setpoint and Current velocity";
