244eb74464e45508b53c20afec5f1bbbf5668856
ameyakonk <78075049+ameyakonk@users.noreply.github.com>
2021-10-03 17:29:22 -0400

    Update main.cpp

--- app/main.cpp
+++ app/main.cpp
@@ -6,7 +6,10 @@ using std :: cout;
 using std :: cin;
 
 int main() {
-    ControllerPID p(1 ,1, 1);
+    double PID[3] = {0};
+    cout << "Enter PID values" << endl;
+    cin >> PID[0] >> PID[1] >> PID[2];
+    ControllerPID p(PID[0] ,PID[1], PID[2]);
     double sp = 0, cv = 0;
     cout << "Enter Setpoint and Current velocity";
     cin >> sp >> cv;
