a04f5e99b2b4fd20e132273ff6b561ecc88d8cda
aditiramadwar <aditiramadwar@hotmail.com>
2021-10-04 10:39:48 -0400

    Doxygen comments

--- app/main.cpp
+++ app/main.cpp
@@ -3,11 +3,33 @@
 #include <pid_lib.hpp>
 using std :: cout;
 using std :: cin;
+using std :: endl;
+
+/**
+ * @def     main.cpp(void)
+ * @brief   Create an instance PID object with initial and target velocities.
+ * call the method computeVelocity of ControllerPID class and print the value
+ * of new velocity.
+ *  
+ * @param1 pid_var[3] Contains the values of the three gains
+ * @param2 sp set target velocity variable
+ * @param3 cv current velocity variable
+ * 
+ * @return int 
+ */
 int main() {
-    double pid_var[3] = {0}, sp = 0, cv = 0;                  
-    cout << "Enter PID values, setpoint and current velocity";
-    cin >> pid_var[0] >> pid_var[1] >> pid_var[2] >> sp >> cv ;         // Enter kd, ki, kd values
-    ControllerPID p(pid_var[0] ,pid_var[1], pid_var[2]);                // Create instance for ControllerPID class
-    cout << p.computeVelocity(sp, cv);
+    // Initialize local pid gain variables
+    double pid_var[3] = {0}, sp = 0, cv = 0;
+    cout << "Enter PID values(Kp, Ki, Kd): " << endl;
+    // Take user input for kd, ki, kd values
+    cin >> pid_var[0] >> pid_var[1] >> pid_var[2];
+    cout << "Enter setpoint and current velocity: " << endl;
+    // Take user input for the target and current velocity
+    cin >> sp >> cv;
+
+    // Create instance for ControllerPID class
+    ControllerPID p(pid_var[0], pid_var[1], pid_var[2]);
+    // Print the new velocity
+    cout << "New Velocity: " << p.computeVelocity(sp, cv) << endl;
     return 0;
 }
